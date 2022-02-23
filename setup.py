a2cbf9063b2e9aa44d1eced3754336064019628c
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-02-23 17:26:01 +0000

    setup: make a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="opt_adapt",
+    description="PDE-constrained optimisation using mesh adaptive methods",
+    author="Joseph Wallwork",
+    author_email="j.wallwork16@imperial.ac.uk",
+    url="https://github.com/pyroteus/pyroteus",
+    version="0.1",
+    packages=["opt_adapt"],
+)
