36af03d978cebf798d32ad201022f05497dc034b
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-07-04 08:55:45 +0100

    setup: fix url

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="PDE-constrained optimisation using mesh adaptive methods",
     author="Joseph Wallwork",
     author_email="j.wallwork16@imperial.ac.uk",
-    url="https://github.com/pyroteus/pyroteus",
+    url="https://github.com/pyroteus/opt_adapt",
     version="0.1",
     packages=["opt_adapt"],
 )
