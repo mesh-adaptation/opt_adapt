8fa7385dfadcb4144f54c7be2bef888403821b90
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-07-08 14:32:20 +0100

    utils: create_directory

--- opt_adapt/utils.py
+++ opt_adapt/utils.py
@@ -1,3 +1,22 @@
 from firedrake.petsc import PETSc
+import os
+
 
 pprint = PETSc.Sys.Print
+
+
+def create_directory(path, comm=PETSc.COMM_WORLD):
+    """
+    Create a directory on disk.
+
+    Code copied from `Thetis
+    <https://thetisproject.org>`__.
+
+    :arg path: path to the directory
+    :kwarg comm: MPI communicator
+    """
+    if comm.rank == 0:
+        if not os.path.exists(path):
+            os.makedirs(path)
+    comm.barrier()
+    return path
