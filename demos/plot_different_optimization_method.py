d0fe313f5e09f991ac780d71b0f2d1ff5ff95e89
acse-yl1321 <yl1321@ic.ac.uk>
2022-07-21 16:53:21 +0100

    plot the convergence with different method

---
+++ demos/plot_different_optimization_method.py
@@ -0,0 +1,50 @@
+from opt_adapt.opt import _implemented_methods
+from opt_adapt.utils import create_directory
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+pwd = os.path.abspath(os.path.dirname(__file__))
+choices = [name for name in os.listdir(pwd) if os.path.isdir(name)]
+parser.add_argument("demo", type=str, choices=choices)
+parser.add_argument("--n", type=int, default=1)
+args = parser.parse_args()
+demo = args.demo
+n = args.n
+plot_dir = create_directory(f"{demo}/plot_different_optimization_method")
+
+fig, axes = plt.subplots()
+for method in _implemented_methods:
+<<<<<<< Updated upstream:demos/plot_convergence.py
+    J = np.load(f"{demo}/data/uniform_progress_J_{n}_{method}.npy")
+=======
+    J = np.load(f"{demo}/data/{run}_progress_J_{n}_{method}.npy")
+    t = np.load(f"{demo}/data/{run}_progress_t_{n}_{method}.npy")
+    times = [sum(t[:i]) for i in range(len(t))]
+    axes.loglog(times, J, label=method)
+axes.grid(True)
+axes.set_title(f"{demo} with mesh adaptation by opt_{run}.py")
+axes.set_xlabel("Cumulative CPU time (seconds)")
+axes.set_ylabel("Objective function value")
+axes.legend()
+plt.tight_layout()
+plt.savefig(f"{plot_dir}/qoi_vs_time_{run}_{n}.png")
+plt.close()
+fig, axes = plt.subplots()
+for method in _implemented_methods:
+    J = np.load(f"{demo}/data/{run}_progress_J_{n}_{method}.npy")
+>>>>>>> Stashed changes:demos/plot_different_optimization_method.py
+    it = np.array(range(len(J))) + 1
+    axes.loglog(it, J, label=method)
+axes.grid(True)
+axes.set_title(f"{demo} with mesh adaptation by opt_{run}.py")
+axes.set_xlabel("Iteration")
+axes.set_ylabel("Objective function value")
+axes.legend()
+plt.tight_layout()
+plt.savefig(f"{plot_dir}/convergence_{n}.png")
