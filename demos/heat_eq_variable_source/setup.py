8846e80aa74ee2559df4535027d803e5c8946d60
acse-yl1321 <yl1321@ic.ac.uk>
2022-07-25 15:32:21 +0100

    Create  a heat_equation example with variable source

---
+++ demos/heat_eq_variable_source/setup.py
@@ -0,0 +1,67 @@
+from firedrake import *
+from firedrake_adjoint import *
+from pyroteus.metric import *
+from pyroteus.recovery import *
+from opt_adapt.opt import get_state
+
+
+def initial_mesh(n):
+    """
+    Construction of initial mesh
+    """
+    return RectangleMesh(5 * 2**n, 5 * 2**n, (-1, 1), (-1, 1))
+
+
+def initial_control(mesh):
+    """
+    The initial of control parameter
+    In this example, control parameter is not in Real space
+    """
+    V = FunctionSpace(mesh, "CG", 1)
+    return Function(V, name="Control")
+
+
+def forward_run(mesh, control, **kwargs):
+    """
+    Solve the PDEs in the given mesh
+    """
+    V = FunctionSpace(mesh, "CG", 1)
+    u = Function(V, name="State")
+    v = TestFunction(V)
+    m = Function(V, name="Control").assign(control)
+
+    # Run the forward model once to create the simulation record
+    F = (inner(grad(u), grad(v)) - m * v) * dx
+    bc = DirichletBC(V, 0.0, "on_boundary")
+    sp = {
+    "mat_type": "aij",  # Matrix type that we need to use a direct solver
+    "snes_monitor": None, # Print the nonlinear solver progress
+    "ksp_type": "preonly",  # Don't use a linear solver, just apply a preconditioner
+    "ksp_monitor": None,  # Print the linear solver progress
+    "pc_type": "lu",  # Use a full LU decomposition as a preconditioner
+    "pc_factor_mat_solver_type": "mumps",  # Use the MUMPS package to compute the LU decomposition
+    }
+    solve(F == 0, u, bc, solver_parameters=sp)
+
+    # The functional of interest is the normed difference between desired
+    # and simulated temperature profile
+    x = SpatialCoordinate(mesh)
+    u_desired = exp(-1 / (1 - x[0] * x[0]) - 1 / (1 - x[1] * x[1]))
+    nrm = assemble(abs(u_desired) * dx)
+    J = assemble(0.5 * inner(u - u_desired, u - u_desired) / nrm * dx)
+
+    return J, m
+
+
+def hessian(mesh, **kwargs):
+    """
+    Recover the Hessian of the state.
+
+    :kwarg adjoint: If ``True``, recover the
+        Hessian of the adjoint state, rather
+        than the forward one.
+    """
+    c = get_state(**kwargs)
+    H = hessian_metric(recover_hessian(c))
+    M = space_normalise(H, 1000.0, "inf")
+    return M
