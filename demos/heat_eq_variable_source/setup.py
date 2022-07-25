217386638f17b75842ba7c738e2e3e4ec436dcc7
acse-yl1321 <yl1321@ic.ac.uk>
2022-07-25 16:17:15 +0100

    indent the parameters

--- demos/heat_eq_variable_source/setup.py
+++ demos/heat_eq_variable_source/setup.py
@@ -34,12 +34,12 @@ def forward_run(mesh, control, **kwargs):
     F = (inner(grad(u), grad(v)) - m * v) * dx
     bc = DirichletBC(V, 0.0, "on_boundary")
     sp = {
-    "mat_type": "aij",  # Matrix type that we need to use a direct solver
-    "snes_monitor": None, # Print the nonlinear solver progress
-    "ksp_type": "preonly",  # Don't use a linear solver, just apply a preconditioner
-    "ksp_monitor": None,  # Print the linear solver progress
-    "pc_type": "lu",  # Use a full LU decomposition as a preconditioner
-    "pc_factor_mat_solver_type": "mumps",  # Use the MUMPS package to compute the LU decomposition
+        "mat_type": "aij",  # Matrix type that we need to use a direct solver
+        "snes_monitor": None, # Print the nonlinear solver progress
+        "ksp_type": "preonly",  # Don't use a linear solver, just apply a preconditioner
+        "ksp_monitor": None,  # Print the linear solver progress
+        "pc_type": "lu",  # Use a full LU decomposition as a preconditioner
+        "pc_factor_mat_solver_type": "mumps",  # Use the MUMPS package to compute the LU decomposition
     }
     solve(F == 0, u, bc, solver_parameters=sp)
 
