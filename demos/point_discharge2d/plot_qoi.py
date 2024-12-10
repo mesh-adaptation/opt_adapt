0b64610da53095055748fb3147963b14ed930d4d
Joe Wallwork <joe.wallwork@outlook.com>
2024-12-10 09:10:13 +0000

    Plot against control, too

--- demos/point_discharge2d/plot_qoi.py
+++ demos/point_discharge2d/plot_qoi.py
@@ -23,22 +23,19 @@ def plot_progress(axes, method, ext, gradients=True, **kwargs):
 labels = {
     "uniform": "Uniform meshing",
     "hessian": "Hessian-based",
-    "go": "Goal-based",
+    # "go": "Goal-based",
 }
 
 # Plot QoI vs #elements
 fig, axes = plt.subplots(figsize=(5, 3))
 for method, label in labels.items():
-    try:
-        nc = []
-        J = []
-        for fname in glob.glob(f"data/{method}_*.log"):
-            ext = "_".join(fname.split("_")[1:])[:-4]
-            nc.append(np.load(f"data/{method}_progress_nc_{ext}.npy")[-1])
-            J.append(np.load(f"data/{method}_progress_J_{ext}.npy")[-1])
-        axes.semilogx(nc, J, "x", label=label)
-    except FileNotFoundError:
-        continue
+    nc = []
+    J = []
+    for fname in glob.glob(f"data/{method}_*.log"):
+        ext = "_".join(fname.split("_")[1:])[:-4]
+        nc.append(np.load(f"data/{method}_progress_nc_{ext}.npy")[-1])
+        J.append(np.load(f"data/{method}_progress_J_{ext}.npy")[-1])
+    axes.semilogx(nc, J, "x", label=label)
 axes.set_xlabel("Number of mesh elements")
 axes.set_ylabel("QoI (dimensionless)")
 axes.grid(True, which="both")
@@ -68,3 +65,42 @@ axes.legend()
 plt.tight_layout()
 plt.savefig("plots/converged_qoi_vs_time.pdf")
 plt.savefig("plots/converged_qoi_vs_time.jpg")
+
+# Plot control vs #elements
+fig, axes = plt.subplots(figsize=(5, 3))
+for method, label in labels.items():
+    nc = []
+    m = []
+    for fname in glob.glob(f"data/{method}_*.log"):
+        ext = "_".join(fname.split("_")[1:])[:-4]
+        nc.append(np.load(f"data/{method}_progress_nc_{ext}.npy")[-1])
+        m.append(np.load(f"data/{method}_progress_m_{ext}.npy")[-1])
+    axes.semilogx(nc, m, "x", label=label)
+axes.set_xlabel("Number of mesh elements")
+axes.set_ylabel(r"Control ($\mathrm{m}$)")
+axes.grid(True, which="both")
+axes.legend()
+plt.tight_layout()
+plt.savefig("plots/converged_control_vs_elements.pdf")
+plt.savefig("plots/converged_control_vs_elements.jpg")
+
+# Plot QoI vs CPU time
+fig, axes = plt.subplots(figsize=(5, 3))
+for method, label in labels.items():
+    try:
+        cpu = []
+        m = []
+        for fname in glob.glob(f"data/{method}_*.log"):
+            ext = "_".join(fname.split("_")[1:])[:-4]
+            cpu.append(np.load(f"data/{method}_progress_t_{ext}.npy")[-1])
+            m.append(np.load(f"data/{method}_progress_m_{ext}.npy")[-1])
+        axes.semilogx(cpu, m, "x", label=label)
+    except FileNotFoundError:
+        continue
+axes.set_xlabel(r"CPU time ($\mathrm{s}$)")
+axes.set_ylabel(r"Control ($\mathrm{m}$)")
+axes.grid(True, which="both")
+axes.legend()
+plt.tight_layout()
+plt.savefig("plots/converged_control_vs_time.pdf")
+plt.savefig("plots/converged_control_vs_time.jpg")
